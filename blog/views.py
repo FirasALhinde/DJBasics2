202de86ad426a71ad320d81c67bbe0a5c28071f9
FirasALhinde <firasalhinde98@gmail.com>
2022-07-02 15:15:26 +0300

    add blog views,templates urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from.models import Post
 # Create your views here.
+
+
+def post_list(request):
+    all_post= Post.objects.all()
+    context ={'posts':all_post}
+    return render(request,'blog/post_list.html',context)
+
+def post_detail(request,id):
+    post=Post.objects.get(id=id)
+    return render(request,'blog/post_detail.html',{'post':post})
